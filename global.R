3a73ebe28d68a3453d11a0e81774b3c18e8d3235
Amy Tzu-Yu Chen <amy17519@users.noreply.github.com>
2016-08-05 16:34:51 -0400

    Update global.R

--- global.R
+++ global.R
@@ -4,8 +4,7 @@ library(dplyr)
 library(googleVis)
 library(DT)
 
-
-kid <- read.csv(url("https://github.com/amy17519/NYCPre-KGuide/blob/master/kid.csv"),
+kid <- read.csv("https://raw.githubusercontent.com/amy17519/NYCPre-KGuide/master/kid.csv",
                 stringsAsFactors=FALSE)
 colnames(kid)[c(2,4,5,6,11,14,16)] <- c("Type","Name",'Phone',
                                         'Address','Zipcode','Meal',
@@ -13,5 +12,6 @@ colnames(kid)[c(2,4,5,6,11,14,16)] <- c("Type","Name",'Phone',
 kidtable<-kid[,-c(1,7,8,9,10)]
 pal = colorFactor( c('#C47027','#1D3557' ,'#db154e'), kid$Type)
 
-boroughkid <- read.csv(url("https://github.com/amy17519/NYCPre-KGuide/blob/master/boroughkid.csv"),stringsAsFactors=FALSE)
+boroughkid <- read.csv("https://raw.githubusercontent.com/amy17519/NYCPre-KGuide/master/boroughkid.csv",
+                       stringsAsFactors=FALSE)
 colnames(boroughkid)<-c("","Borough","Seat Avalibility","2014 Estimate","2010 Census")
